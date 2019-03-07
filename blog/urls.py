2753a9925690cb949a78c7ce0b2c0c49aa72f736
QuatreMains <plutokor@gmail.com>
2019-03-08 04:50:10 +0900

    first commit

---
+++ blog/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+import blog.views
+
+urlpatterns = [
+    path('', blog.views.home, name="home"),
+    path('blog/<int:blog_id>/', blog.views.detail, name="detail"),
+    path('blog/new/', blog.views.new, name="new"),
+    path('blog/create/', blog.views.create, name='create'),
+]
